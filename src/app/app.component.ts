5a883a6327c5ea819284e5b246f72104988325bb
kunal2899 <kunaljain649@gmail.com>
2021-03-06 12:16:51 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PortFolio';
+  constructor(){
+    jQuery(function () {
+      $('nav .nav_links').children().on('click',function(){
+        let left = $(this).offset().left - $('nav').offset().left
+        let width = $(this).css('width')
+        $('.contact').removeClass('onit');
+        $(this).children('a').addClass('active')
+        $(this).siblings().children('a').removeClass('active')
+        $('.marker').css('opacity',1).css('width',width).css('left',left)
+      });
+      $('.contact').on('click',function(){
+        $(this).addClass('onit');
+        $('nav .nav_links').children().children().removeClass('active');
+        $('.marker').css('width',0).css('opacity',0);
+      })
+    });
+  }
 }
